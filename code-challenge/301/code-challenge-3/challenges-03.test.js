1527217ed2c18b5bed8f9d7c654eb8b127499bcd
Ed Dearment <ed787.ed@gmail.com>
2019-04-24 17:03:27 -0700

    challenge 5 done

--- code-challenge/301/code-challenge-3/challenges-03.test.js
+++ code-challenge/301/code-challenge-3/challenges-03.test.js
@@ -52,7 +52,14 @@ Here is an example of the input:
 ------------------------------------------------------------------------------------------------ */
 
 const sortByPrice = (arr) => {
-  // Solution code here...
+  arr.sort( (a,b) => {
+    if (a.price < b.price){
+      return -1;
+    } else if (a.price > b.price) {
+      return 1;
+    }
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
