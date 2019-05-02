9faab661f9589ee64e390a38530f0f845ae33c47
ekearm <ed787.ed@gmail.com>
2019-05-01 19:31:48 -0700

    challenge 5 done

--- code-challenge/301/code-challenge-8/challenges-08.test.js
+++ code-challenge/301/code-challenge-8/challenges-08.test.js
@@ -82,7 +82,14 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 ------------------------------------------------------------------------------------------------ */
 
 const getStatName = (arr, minBaseStat) => {
-  // Solution code here...
+    let nameArr = []
+    arr.filter( el => {
+        if(el.baseStat > minBaseStat){
+            nameArr.push(el.stat.name);
+        }
+    });
+    return nameArr;
+
 };
 
 /* ------------------------------------------------------------------------------------------------
