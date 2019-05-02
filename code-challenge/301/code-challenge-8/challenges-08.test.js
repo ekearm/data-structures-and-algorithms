943128ac7f87fc44f5e1f226095f2e21e4b9712f
ekearm <ed787.ed@gmail.com>
2019-05-01 19:15:18 -0700

    challenge 2 done

--- code-challenge/301/code-challenge-8/challenges-08.test.js
+++ code-challenge/301/code-challenge-8/challenges-08.test.js
@@ -19,7 +19,8 @@ For example, filterStringsWithVowels('gregor','hound','xyz') returns ['gregor', 
 
 
 const filterStringsWithVowels = (arr) => {
-  // Solution code here...
+  let reg = /[aeiou]/g;
+  return arr.filter(el => el.match(reg));
 };
 
 
