60f7241a781452ed77e65d184d086a3f56691746
ekearm <ed787.ed@gmail.com>
2019-05-01 19:26:58 -0700

    challenge 4 done

--- code-challenge/301/code-challenge-8/challenges-08.test.js
+++ code-challenge/301/code-challenge-8/challenges-08.test.js
@@ -72,7 +72,7 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
+    return arr.filter( el => el.baseStat > minBaseStat);
 };
 
 /* ------------------------------------------------------------------------------------------------
