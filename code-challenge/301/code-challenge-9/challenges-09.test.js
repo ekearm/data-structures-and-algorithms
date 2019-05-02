ec5b1cc721e8efdbf8e303044e05b8aadeb2e8c4
ekearm <ed787.ed@gmail.com>
2019-05-02 16:33:58 -0700

    challenge 1 done

--- code-challenge/301/code-challenge-9/challenges-09.test.js
+++ code-challenge/301/code-challenge-9/challenges-09.test.js
@@ -7,7 +7,7 @@ Note: You may not use the array's built-in length property.
 ------------------------------------------------------------------------------------------------ */
 
 const countNumberOfElements = (arr) => {
-  // Solution code here...
+    return arr.reduce( (acc, curr, ind) => ind + 1);
 };
 
 /* ------------------------------------------------------------------------------------------------
