b0cfe2082762554dd3261f9f1369cb865d4cf8b4
ekearm <ed787.ed@gmail.com>
2019-05-06 20:54:00 -0700

    challenge1 passed

--- code-challenge/301/code-challenge-11/challenges-11.test.js
+++ code-challenge/301/code-challenge-11/challenges-11.test.js
@@ -7,7 +7,7 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePin = (pin) => {
-  // Solution code here...
+    return /\b\d{4}\b/g.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
