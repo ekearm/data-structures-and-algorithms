cf078b97d55598b11d811bc48618a0300ed0dee8
ekearm <ed787.ed@gmail.com>
2019-05-21 08:08:33 -0700

    better solution

--- code-challenge/401/arrayReverse/array-reverse.js
+++ code-challenge/401/arrayReverse/array-reverse.js
@@ -6,7 +6,7 @@ let n = myArr.length;
 let i = 0;
 let j = 0;
 
-for (i = 0,j = 9; i < n/2; i++, j-- ){
+for (i = 0,j = (n-1); i < n/2; i++, j-- ){
     temp = myArr[i];
     myArr[i] = myArr[j];
     myArr[j] = temp;
