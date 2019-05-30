f8b642f675a6bd09fe8e9e15dfe5f4d81c7dd28d
ekearm <ed787.ed@gmail.com>
2019-05-30 11:40:34 -0700

    linked list work

---
+++ Data-Structures/linkedList/linked-list.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const linkedList = require('./linked-list.js');
+
+describe ('Test da liss wit Nodemon!'), () => {
+  it('it should create a list', () => {
+    let myList = new linkedList();
+    expect(list.head).toEqual(null);
+  });
+}
