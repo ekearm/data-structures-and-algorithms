41eeb9d5f831b843d70519abc6b0295d1b1d80dc
ekearm <ed787.ed@gmail.com>
2019-06-09 19:26:25 -0700

    stacks done

---
+++ Data-Structures/Stack/__tests__/stacks.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const stack = require('../stack');
+
+describe('Stack Module', () => {
+  let stack;
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  describe('constructor', () => {
+    it('creates a new stack', () => {
+      expect(Stack).toBeDefined();
+
+    })
+  })
+});
