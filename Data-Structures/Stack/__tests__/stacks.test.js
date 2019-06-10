4ea0982a66f29c8a43d1cb01548201f37db7fdcb
ekearm <ed787.ed@gmail.com>
2019-06-09 19:38:45 -0700

    finished queues

--- Data-Structures/Stack/__tests__/stacks.test.js
+++ Data-Structures/Stack/__tests__/stacks.test.js
@@ -1,17 +1,55 @@
 'use strict';
 
-const stack = require('../stack');
+const Stack = require('../stack');
 
-describe('Stack Module', () => {
+describe('Stack and Queues', () => {
+
   let stack;
-  beforeEach(() => {
-    stack = new Stack();
+
+  describe('Name of the group', () => {
+
+    beforeEach(() => {
+      stack = new Stack();
+    });
+
+
+    it('Can successfully push onto a stack', () => {
+      stack.push(5);
+      expect(stack.top.value).toBe(5);
+    });
+
+    it('Can successfully push multiple nodes onto a stack', () => {
+      stack.push(4);
+      stack.push(5);
+      stack.push(6);
+      stack.push(7);
+      expect(stack.top.value).toBe(7);
+    });
+
+    it('Can successfully pop off the stack', () => {
+      stack.push(4);
+      stack.push(5);
+      stack.pop();
+      expect(stack.top.value).toBe(4);
+    });
+
+    it('Can successfully empty a stack after multiple pops', () => {
+      stack.push(4);
+      stack.push(5);
+      stack.pop();
+      stack.pop();
+      expect(stack.top).toBeNull();
+    });
+
+    it('Can successfully peek the next item on the stack.', () => {
+      stack.push(4);
+      stack.push(5);
+      expect(stack.peek()).toBe(5);
+    });
+
+    it('Can successfully instantiate an empty stack', () => {
+      expect(stack).toBeInstanceOf(Stack);
+      expect(stack.peek()).toBeNull();
+    });
   });
-
-  describe('constructor', () => {
-    it('creates a new stack', () => {
-      expect(Stack).toBeDefined();
-
-    })
-  })
 });
