b57f984e3a2ef8510bdc7608cbd314106a6ad67f
ekearm <ed787.ed@gmail.com>
2019-07-02 18:42:07 -0700

    test 3 passed

--- Data-Structures/mergeSort/__tests__/mergeSort.test.js
+++ Data-Structures/mergeSort/__tests__/mergeSort.test.js
@@ -15,6 +15,22 @@ describe('the merge sort function', () => {
     expect(sorted).toEqual([2, 3, 4, 5, 7, 8]);
   });
 
-  
+  it('Should sort an array with 1 value', () => {
+    let arr = [3];
+    let sorted = mergeSort(arr);
+    expect(sorted).toEqual([5]);
+    expect(sorted.length).toEqual(1);
+  });
 
-})
+  it('Should not modify the array', () => {
+    let arr = [2,4,8,5,7,3];
+    let sorted = mergeSort(arr);
+    expect(sorted.length).toEqual(6);
+    expect(sorted).toContain(5);
+    expect(sorted).toContain(7);
+    expect(sorted).toContain(8);
+    expect(sorted).toContain(4);
+    expect(sorted).toContain(3);
+    expect(sorted).toContain(2);
+  });
+});
