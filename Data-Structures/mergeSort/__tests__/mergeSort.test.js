49c6a181027a9ff7f1387beb707c3d0464810803
ekearm <ed787.ed@gmail.com>
2019-07-02 18:38:09 -0700

    2nd test passed

--- Data-Structures/mergeSort/__tests__/mergeSort.test.js
+++ Data-Structures/mergeSort/__tests__/mergeSort.test.js
@@ -4,13 +4,17 @@ let mergeSort = require('..mergeSort');
 
 describe('the merge sort function', () => {
   it('Should sort an array with an odd amount of values', () => {
-    // arrange
     let arr = [5, 7, 8, 4, 3, 2, 9];
-
-    // act
     let sorted = mergeSort(arr);
-
-    // assert
     expect(sorted).toEqual([2, 3, 4, 5, 7, 8, 9]);
   });
+
+  it('Should sort an array with an even amount of values', () => {
+    let arr = [5, 7, 8, 4, 3, 2];
+    let sorted = mergeSort(arr);
+    expect(sorted).toEqual([2, 3, 4, 5, 7, 8]);
+  });
+
+  
+
 })
