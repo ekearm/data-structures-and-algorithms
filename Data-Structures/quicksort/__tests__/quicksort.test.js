f2cdb15c6e049c00e29c05af7a14d6d61fc074ef
ekearm <ed787.ed@gmail.com>
2019-07-03 22:27:00 -0700

    second test passed

--- Data-Structures/quicksort/__tests__/quicksort.test.js
+++ Data-Structures/quicksort/__tests__/quicksort.test.js
@@ -16,4 +16,11 @@ describe('quickSort', () => {
     expect(testArray[4]).toBe(12);
   });
 
+  it('should throw an error if an element is not number or cant be coerced into a number', () => {
+    let array = [9, 8, 'Hello!!!'];
+    expect(() => {
+      quickSort(array, 0, array.length - 1)(array).toThrow();
+    });
+  });
+
 });
